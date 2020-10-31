242e8f17f97cfcda1da869f10599c1f438e37f20
conor-mcnally <cmcn@gmx.co.uk>
2020-10-31 04:36:15 +0000

    Even prettier print

--- CONVERTstringNUMBERtoWORDS.py
+++ CONVERTstringNUMBERtoWORDS.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
 object = "12.09.2020"
-print("\033[1m" + "\nExample input: " + "\033[0m", object)
+print("\033[1m" + "\nExample input: " + "\033[0m\n", object)
 
 #First convert to valid datetime object by using correct % operator
 #forhow they are currently formatted
 #See https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
 #for full list
 print("\033[1m" + "\nCode snippet to convert example object to datetime object"+ "\033[0m")
-print("\033[4m" + "object = datetime.strptime(object, %d.%m.%Y)" + "\033[0m")
+print("\033[3m" + "object = datetime.strptime(object, %d.%m.%Y)" + "\033[0m")
 
 object = datetime.strptime(object, "%d.%m.%Y")
 
-print("\033[1m" + "\nFormat of datetime object"+ "\033[0m", object, "\n")
+print("\033[1m" + "\nFormat of datetime object\n"+ "\033[0m", object, "\n")
 
 
 #Now that it is in a datetime object, we can extract words from it
