5b8bc6d26cc5af4124ef3e8c7603a2ad39163328
conor-mcnally <cmcn@gmx.co.uk>
2020-11-01 04:22:41 +0000

    update

--- analysis.py
+++ analysis.py
@@ -1,22 +1,34 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
+import pandas as pd
+from datetime import datetime
 
-with open('workHours.csv') as csv_file:
-    return
+
 
 df = pd.read_csv('workHours.csv')
-#df.at['SumPay', 'Total'] = df['Total'].sum()
-df.sort_values('Date')
-df['Date'] = [date.weekday() for date in df['Date']]
+# df.at['SumPay', 'Total'] = df['Total'].sum()
+
+df['Date'] = pd.to_datetime(df['Date']).dt.strftime("%d.%m.%y")
+df['Day'] = pd.to_datetime(df['Date']).dt.strftime('%A')
+df['Month'] = pd.to_datetime(df['Date']).dt.strftime('%B')
+print(df)
+df.plot(x='Day', y='Total')
+plt.show()
+
 
 #Id want the groups to be Days
 #Id want the diff bars be months
 
-plt.figure(figsize=(13, 5))
-plt.title("Avg pay per day per month")
-plt.ylabel("£££")
-plt.xlabel("Day")
-plt.bar(df['Date'], df['Total'])
-plt.show()
-print(df)
+
+# plt.title('Pay per month Avg')
+# plt.ylabel('£')
+# plt.xlabel('Days')
+# for i in df:
+#     plt.plot(df['Day'], df['Total'])
+
+# plt.show()
+# plt.figure(figsize=(13, 5))
+# plt.title("Avg pay per day per month")
+# plt.ylabel("£££")
+# plt.xlabel("Day")
+# plt.bar(df['Date'], df['Total'])
+# plt.show()
