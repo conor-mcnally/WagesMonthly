358a93ff9eff07f9a3c12e590a9ac3f243e393b6
conor-mcnally <cmcn@gmx.co.uk>
2020-11-01 00:48:57 +0000

    data

---
+++ analysis.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
+df = pd.read_csv('workHours.csv')
+#df.at['SumPay', 'Total'] = df['Total'].sum()
+df.sort_values('Date')
+df['Date'] = [date.weekday() for date in df['Date']]
+
+#Id want the groups to be Days
+#Id want the diff bars be months
+
+plt.figure(figsize=(13, 5))
+plt.title("Avg pay per day per month")
+plt.ylabel("£££")
+plt.xlabel("Day")
+plt.bar(df['Date'], df['Total'])
+plt.show()
+print(df)
