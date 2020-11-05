6892786c1bcc098b24333b4dc8956c6fb00a9e4f
conor-mcnally <cmcn@gmx.co.uk>
2020-11-05 04:42:40 +0000

    minor fixes

--- analysis.py
+++ analysis.py
@@ -8,17 +8,15 @@ df = pd.read_csv('workHours.csv')
 #Convert dates to datetime object
 #Then convert to individual date values
 df['Date'] = pd.to_datetime(df['Date']).dt.strftime("%d.%m.%y")
-df['Day'] = pd.to_datetime(df['Date']).dt.strftime('%A')
+df['Day'] = pd.to_datetime(df['Date']).dt.strftime('%a')
 df['Month'] = pd.to_datetime(df['Date']).dt.strftime('%B')
-df = df.sort_values(['Month'])
-
 
 #Aggregate mean by month & day
-groupby2 = df.groupby(['Month', 'Day'])['Total'].agg(['mean']).reset_index()
+df = df.groupby(['Month', 'Day'])['Total'].agg(['mean']).reset_index() #Deleting reset index will break
 
-#Pivot, get the data in form that makes it
-#easy to plot group value
-fig = groupby2.pivot(index="Day", columns="Month", values='mean').plot(kind='bar', figsize=(12, 6))
+#Pivot, get the data in form that makes it easy to plot group value
+#Save in variable fig to plot in GUI
+fig = df.pivot(index="Day", columns="Month", values='mean').plot(kind='bar', figsize=(12, 6))
 plt.title('Mean pay by day per month')
 plt.xlabel('Day')
 plt.ylabel('Amount (£)')
